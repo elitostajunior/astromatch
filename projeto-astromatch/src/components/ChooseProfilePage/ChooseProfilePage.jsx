2ec5be6eaa26160e1fc5f6126418e1c81426a2d7
eligtj <eli_gtj@hotmail.com>
2023-08-10 21:18:01 -0300

    feito lógica de estado - interação banco de dados

--- projeto-astromatch/src/components/ChooseProfilePage/ChooseProfilePage.jsx
+++ projeto-astromatch/src/components/ChooseProfilePage/ChooseProfilePage.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProfileCard from './ProfileCard'
 import ChooseButtons from './ChooseButtons'
+import axios from 'axios'
 
 function ChooseProfilePage() {
-  const profileToChoose = {
-    "id": "xUrxMGvODWZa4ZASbfwx",
-    "age": 26,
-    "name": "Carol Danvers",
-    "photo": "https://s1.r29static.com/bin/entry/7e8/340x408,85/2138124/image.webp",
-    "bio": "Gosto de voar e de gatos. Procuro relações que respeitem minha independência."
-  }
+  const [profileToChoose, setProfileToChoose] = useState({})
+
+  useEffect(() => {
+    axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/eli-junior-joy/person')
+    .then(response => {
+      setProfileToChoose(response.data.profile)
+    })
+  }, [])
 
   return (
     <div>
--- projeto-astromatch/src/components/MatchListPage/MatchListPage.jsx
+++ projeto-astromatch/src/components/MatchListPage/MatchListPage.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MatchListItem from './MatchListItem'
 import styled from 'styled-components'
+import axios from 'axios'
 
 const ListContainer = styled.div`
   padding: 8px;
 `
 
 function MatchListPage() {
-  const matches = [
-    {
-      "id": "71gMbZs2txS9LDvGK5Ew",
-      "age": 26,
-      "name": "Anitta",
-      "photo": "https://www.polemicaparaiba.com.br/wp-content/uploads/2023/02/anitta-perde-artista-revelacao-grammy-2023.jpg",
-      "bio": "Amo cachorros e sair para dançar. Procuro alguém animado e sem neuras."
-    }
-  ]
+  const [matches, setMatches] = useState([])
+
+  useEffect(() => {
+    axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/eli-junior-joy/matches')
+    .then(response => {
+      setMatches(response.data.matches)
+    })
+  }, [])
 
   return (
     <ListContainer>
