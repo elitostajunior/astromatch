087c0c34446657776e903cf98f9dc1e23e0932eb
eligtj <eli_gtj@hotmail.com>
2023-10-10 15:04:00 -0300

    feito ajuste de estilização botão resetar curtidas

--- projeto-astromatch/src/components/ResetButton/ResetButton.jsx
+++ projeto-astromatch/src/components/ResetButton/ResetButton.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const ResetButtonStyle = styled.div`
+  display: flex;
+  margin-top: 3vh;
+  justify-content: center;
+`
 
 function ResetButton() {
   const onClickReset = () => {
@@ -10,9 +17,9 @@ function ResetButton() {
   }
 
   return (
-    <div>
+    <ResetButtonStyle>
       <button onClick={onClickReset}>Resetar curtidas e matches</button>
-    </div>
+    </ResetButtonStyle>
   )
 }
 
